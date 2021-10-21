8680b611f4d53e64b8529f6bcbc59b6b5e36a6aa
sander <sandersoodla@gmail.com>
2021-10-21 19:28:56 +0300

    changes to author name display

--- js/retrieve-posts.js
+++ js/retrieve-posts.js
@@ -14,7 +14,7 @@ $(function() {
             let pictureLink = $('<a href="#">');
             pictureLink.append($('<img src="images/profile-pictures/' + post.author.profilePicture + '" id="poster' + post.id + '-pfp" alt="pfp">'));
             profilePicture.append(pictureLink);
-            profilePicture.append(" " + post.author.firstname + " " + post.author.lastname);
+            profilePicture.append($('<p>').text(post.author.firstname + " " + post.author.lastname));
 
             let postDatetime = $('<div class="post-datetime">');
             postDatetime.append($('<p>').text(new Date(post.date).toLocaleDateString("en-US", dateOptions)));
