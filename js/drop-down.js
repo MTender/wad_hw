cb22b53bbcb85885128aa2b845760d1b3640d881
Märt <mart.tender@ut.ee>
2021-10-22 18:18:07 +0300

    Convert from regular JS to jQuery

--- js/drop-down.js
+++ js/drop-down.js
@@ -1,10 +1,10 @@
-function dropFunction() {
-    console.log("test")
-    let dropdown = document.getElementById('myDropdown');
-    if (dropdown.style.display == "block"){
-        dropdown.style.display="none";
-    }
-    else {
-        dropdown.style.display="block";
-    }
-  }
+$(function() {
+    $('#current-user-pfp').on('click', function() {
+        let dropdown = $('#myDropdown');
+        if (dropdown.css("display") == "none") {
+            dropdown.css("display", "block");
+        } else {
+            dropdown.css("display", "none");
+        }
+    });
+});
